1663aa5cbf2ca7f26110ef63e9679d8c079d8567
Hannes Sverrisson <hannessv@gmail.com>
2019-12-10 19:16:15 +0000

    Initial interface

--- Tests/EtradeAPIModuleTests/EtradeAPIModuleTests.swift
+++ Tests/EtradeAPIModuleTests/EtradeAPIModuleTests.swift
@@ -3,6 +3,16 @@ import EtradeAPIModule
 @testable import EtradeAPIModule
 
 final class EtradeAPIModuleTests: XCTestCase {
+
+    func testKeysLoading() {
+        let name = "api_keys_sample"
+        let keys: API_Keys = loadJSON(name)!
+        XCTAssertNotNil(keys, "Keys nil")
+        XCTAssertNotNil(keys.sandbox, "Keys nil")
+        XCTAssertNotNil(keys.sandbox?.key, "Keys nil")
+        XCTAssertNotNil(keys.sandbox?.secret, "Keys nil")
+    }
+    
     func testKeyImport() {
         let mode: APIMode = .sandbox
         let etrade = EtradeAPIModule(mode: mode)
